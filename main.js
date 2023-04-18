1b7501b83e88cfbaf3ee80eb8940c78aa0323a07
zjoas820 <127806813+zjoas820@users.noreply.github.com>
2023-04-18 11:45:49 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const vertical = document.querySelector(".vertical");
+const horizontal = document.querySelector(".horizontal");
+const target = document.querySelector(".target");
+const tag = document.querySelector(".tag");
+
+addEventListener("load", () => {
+  const targetRect = target.getBoundingClientRect();
+  const targetHalfWidth = targetRect.width / 2;
+  const targetHalfHeight = targetRect.height / 2;
+  let curX = 0;
+  let curY = 0;
+  let mouseX = 0;
+  let mouseY = 0;
+
+  document.addEventListener("mousemove", (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+    console.log(`X좌표값${x} Y좌표값${y}`);
+  });
+
+  function animate() {
+    requestAnimationFrame(animate);
+    curX += (mouseX - curX) * 0.2;
+    curY += (mouseY - curY) * 0.2;
+
+    vertical.style.transform = `translateX(${curX}px)`;
+    horizontal.style.transform = `translateY(${curY}px)`;
+    target.style.transform = `translate(${curX - targetHalfWidth}px, ${curY - targetHalfHeight}px)`;
+    tag.style.transform = `translate(${curX}px, ${curY}px)`;
+    tag.innerHTML = `${Math.ceil(curX)}px, ${Math.ceil(curY)}px`;
+  }
+
+  animate();
+});
